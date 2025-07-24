63c92b011427b81c110c56a8fe7b3a54d7203790
CoretexShadow <ilya.abdolmanafi@gmail.com>
2025-07-24 12:23:48 +0330

    file Strong_Password_Generator.py changed name

---
+++ src/Random_Password_Generator.py
@@ -0,0 +1,34 @@
+from abc import ABC, abstractmethod
+class PasswordGenerator(ABC):
+    @abstractmethod
+    def generate(self):
+        pass
+
+import string
+import random
+
+
+class RandomPasswordGenerator(PasswordGenerator):
+    def __init__ (self, include_numbers : bool = False , include_symbols : bool = False ,length = 8 ):
+        self.include_numbers = include_numbers
+        self.include_symbols = include_symbols
+        self.password = string.ascii_letters
+        self.length = length
+    def generate(self):
+        if self.include_numbers :
+            self.password += string.digits
+        if self.include_symbols :
+            self.password += string.punctuation
+        return "".join(random.choice(self.password) for _ in range(self.length))
+if __name__ == "__main__":
+    try :
+            user_length = int(input("Enter the length of the password"))
+            user_symbols = input("Do you want symbols in your password?, type no if you dont want")
+            user_number = input("Do you want numbers in your password?, type no if you dont want")
+            include_symbols = user_symbols.lower() != "no"
+            include_numbers = user_number.lower() != "no"
+
+            user_password = RandomPasswordGenerator(include_numbers=include_numbers, include_symbols=include_symbols, length=user_length)
+            print(user_password.generate())
+    except Exception as e:
+        print("Error occurred:", e)
